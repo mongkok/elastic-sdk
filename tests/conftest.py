8cbe6082e433617afa5dd3a5f7a03a84f2c4f785
mongkok <domake.io@gmail.com>
2017-08-27 13:29:57 +0200

    Test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,13 @@ import pytest
 @pytest.fixture(scope='module')
 def client(request):
     return elastic_sdk.Client(index_name='test')
+
+
+@pytest.fixture(scope='module')
+def index(request, client):
+    return client.fixtures.idx(
+        id='fixture_id',
+        body={
+            'test': True,
+            'created': None
+        })
--- tests/test_client.py
+++ tests/test_client.py
@@ -3,3 +3,39 @@
 
 def test_client_info(client):
     assert 'name' in client.es.info()
+
+
+def test_search_create_index(client):
+    response = client.search().idx(
+        id='test.id',
+        body={'test': True},
+        doc_type='doc.test')
+
+    assert response['_type'] == 'doc.test'
+
+
+def test_attr_create_index(client):
+    response = client.user.idx(id='user.id', body={'username': 'me'})
+    assert response['_type'] == 'user'
+
+
+def test_client_get(client, index):
+    assert client.get(id=index['_id'])['test']
+
+
+def test_client_delete(client):
+    index = client.fixtures.idx(id='fixture.tmp', body={})
+    response = client.delete(doc_name='fixtures', id=index['_id'])
+    assert response['result'] == 'deleted'
+
+
+def test_search_all(client):
+    assert client.fixtures.all().hits().total
+
+
+def test_search_q(client):
+    assert client.fixtures.q(test=True).hits().total
+
+
+def test_search_exists(client):
+    assert client.fixtures.exists(field='test').hits().total
